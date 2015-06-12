1818d1a8552e13fe0c61e277621da40484bf7a52
wardkennes <dekennes@gmail.com>
2015-06-13 00:37:10 +0200

    Error while running Grunt no such file or directory

---
+++ GruntFile.js
@@ -0,0 +1,105 @@
+/*!
+ * Cribbb Gruntfile
+ * http://cribbb.com
+ * @author Philip Brown
+ */
+
+'use strict';
+
+/**
+ * Grunt Module
+ */
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+
+    /**
+     * Get package meta data
+     */
+    pkg: grunt.file.readJSON('package.json'),
+
+
+    /**
+     * Set project object
+     */
+    project: {
+      app: 'app',
+      assets: '<%= project.app %>/assets',
+      src: '<%= project.assets %>/src',
+      css: [
+        '<%= project.src %>/scss/style.scss'
+      ],
+      js: [
+        '<%= project.src %>/js/*.js'
+      ]
+    },
+
+    /**
+     * Project banner
+     */
+    tag: {
+      banner: '/*!\n' +
+              ' * <%= pkg.name %>\n' +
+              ' * <%= pkg.title %>\n' +
+              ' * <%= pkg.url %>\n' +
+              ' * @author <%= pkg.author %>\n' +
+              ' * @version <%= pkg.version %>\n' +
+              ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+              ' */\n'
+    },
+
+
+    /**
+     * Sass
+     */
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          compass: true
+        },
+        files: {
+          '<%= project.assets %>/css/style.css': '<%= project.css %>'
+        }
+      },
+      dist: {
+        options: {
+          style: 'compressed',
+          compass: true
+        },
+        files: {
+          '<%= project.assets %>/css/style.css': '<%= project.css %>'
+        }
+      }
+    },
+
+
+    /**
+     * Watch
+     */
+    watch: {
+      sass: {
+        files: '<%= project.src %>/scss/{,*/}*.{scss,sass}',
+        tasks: ['sass:dev']
+      }
+    },
+
+
+
+  });
+
+    /**
+ * Load Grunt plugins
+ */
+require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+
+
+  /**
+ * Default task
+ * Run `grunt` on the command line
+ */
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.registerTask('default', ['sass:dev','watch']);
+
+};
