ba2b41e71aa85a74138f85e3d62ede598eb1c1ef
wardkennes <dekennes@gmail.com>
2015-06-17 12:36:05 +0200

    Sass task added for Grunt

--- GruntFile.js
+++ GruntFile.js
@@ -3,11 +3,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     concat: {
       js: {
-        src: ['js/1.js', 'js/2.js'],
+        src: ['js/app.js'],
         dest: 'build/js/script.js',
       },
       css: {
-        src: ['css/main.css', 'css/theme.css'],
+        src: ['css/main.css'],
         dest: 'build/css/styles.css',
       },
     },
@@ -17,14 +17,30 @@ module.exports = function(grunt) {
         tasks: ['concat:js'],
       },
       css: {
-        files: ['css/**/*.css'],
-        tasks: ['concat:css'],
+        files: 'scss/*.scss',
+        tasks: ['sass'],
+      },
+    },
+
+    sass: {
+      options: {
+        compress: false,
+        sourcemap: 'none'
+      },
+      dist: {
+        files: {
+          'css/main.css': 'scss/main.scss',
+        },
       },
     },
   });
 
+  // Load Tasks
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['concat', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Merge tasks into 1 default
+  grunt.registerTask('default', ['concat', 'watch', 'sass']);
 
 };
