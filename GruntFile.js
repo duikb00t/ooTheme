0b81222df828ab8d558bdb247b06c56883804058
wardkennes <dekennes@gmail.com>
2015-06-17 12:14:18 +0200

    Grunt task added

--- GruntFile.js
+++ GruntFile.js
@@ -1,105 +1,30 @@
-/*!
- * Cribbb Gruntfile
- * http://cribbb.com
- * @author Philip Brown
- */
-
-'use strict';
-
-/**
- * Grunt Module
- */
 module.exports = function(grunt) {
+
   grunt.initConfig({
-
-
-    /**
-     * Get package meta data
-     */
-    pkg: grunt.file.readJSON('package.json'),
-
-
-    /**
-     * Set project object
-     */
-    project: {
-      app: 'app',
-      assets: '<%= project.app %>/assets',
-      src: '<%= project.assets %>/src',
-      css: [
-        '<%= project.src %>/scss/style.scss'
-      ],
-      js: [
-        '<%= project.src %>/js/*.js'
-      ]
-    },
-
-    /**
-     * Project banner
-     */
-    tag: {
-      banner: '/*!\n' +
-              ' * <%= pkg.name %>\n' +
-              ' * <%= pkg.title %>\n' +
-              ' * <%= pkg.url %>\n' +
-              ' * @author <%= pkg.author %>\n' +
-              ' * @version <%= pkg.version %>\n' +
-              ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
-              ' */\n'
-    },
-
-
-    /**
-     * Sass
-     */
-    sass: {
-      dev: {
-        options: {
-          style: 'expanded',
-          compass: true
-        },
-        files: {
-          '<%= project.assets %>/css/style.css': '<%= project.css %>'
-        }
+    concat: {
+      js: {
+        src: ['js/1.js', 'js/2.js'],
+        dest: 'build/js/script.js',
       },
-      dist: {
-        options: {
-          style: 'compressed',
-          compass: true
-        },
-        files: {
-          '<%= project.assets %>/css/style.css': '<%= project.css %>'
-        }
-      }
+      css: {
+        src: ['css/main.css', 'css/theme.css'],
+        dest: 'build/css/styles.css',
+      },
     },
-
-
-    /**
-     * Watch
-     */
     watch: {
-      sass: {
-        files: '<%= project.src %>/scss/{,*/}*.{scss,sass}',
-        tasks: ['sass:dev']
-      }
+      js: {
+        files: ['js/**/*.js'],
+        tasks: ['concat:js'],
+      },
+      css: {
+        files: ['css/**/*.css'],
+        tasks: ['concat:css'],
+      },
     },
-
-
-
   });
 
-    /**
- * Load Grunt plugins
- */
-require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-
-
-  /**
- * Default task
- * Run `grunt` on the command line
- */
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.registerTask('default', ['sass:dev','watch']);
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['concat', 'watch']);
 
 };
